674a2b533a917b3d6558bb9f4118ebb6865e741c
Khaile <khai.le@akagidital.vn>
2024-09-20 10:22:10 +0700

    new

---
+++ sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyBN1l85LolRVAMOrQxGQZzXF2KVAEpAo78',
+    messagingSenderId: '157637685959',
+    appId: '1:157637685959:web:20215706b205b2a31c84e2',
+    projectId: 'vieclamclone',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BSUH1PDDQFBSPLEV97HGG26AH0G',
+    siteid: '504ea8ca2cb4400285e24f2d3705750b'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
